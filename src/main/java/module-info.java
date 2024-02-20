bf55dbfa489768637e7505216291b9d7f6cced55
BhavyanshS <126804826+BhavyanshS@users.noreply.github.com>
2024-02-20 18:37:42 +0100

    Name change

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module com.example.demo1 {
     requires javafx.fxml;
 
 
-    opens com.example.demo1 to javafx.fxml;
-    exports com.example.demo1;
-    exports;
-    opens to
+    opens com.program to javafx.fxml;
+    exports com.program;
 }
